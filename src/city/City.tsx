fa717112d474a1f60cc054d70dc86a7681770b8a
evanhosni <evankhaledhosni@gmail.com>
2023-10-15 17:37:19 -0700

    updates to noise

--- src/city/City.tsx
+++ src/city/City.tsx
@@ -5,11 +5,11 @@ import { Biome } from "../_/terrain/biomes";
 
 const city_biome: Biome = {
   noise: {
-    octaves: 1,
+    octaves: 2,
     persistence: 1,
     lacunarity: 1,
     exponentiation: 1,
-    height: 4,
+    height: 10,
     scale: 8,
     // noise_type: string, //TODO bring this back if u ever find a use for having both perlin and simplex
     seed: 1,
