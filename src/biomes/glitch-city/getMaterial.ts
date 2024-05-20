a195e98f2580bcfebb96b7e771ad40b8ef019178
evanhosni <evankhaledhosni@gmail.com>
2024-05-19 19:24:37 -0700

    better typing for Dimension

--- src/biomes/glitch-city/getMaterial.ts
+++ src/biomes/glitch-city/getMaterial.ts
@@ -1,11 +1,11 @@
 import * as THREE from "three";
 import { _material } from "../../_/_material";
 import { Biome } from "../../types/Biome";
-import { GlitchCityDimension } from "./GlitchCity";
+import { GlitchCity } from "./GlitchCity";
 
 export const getMaterial = async () => {
   const biomes = Array.from(
-    GlitchCityDimension.regions.reduce((acc, region) => {
+    GlitchCity.regions.reduce((acc, region) => {
       region.biomes.forEach((biome) => acc.add(biome));
       return acc;
     }, new Set<Biome>())
