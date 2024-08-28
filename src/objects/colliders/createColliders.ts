7b327a44dd290771e8edd8647cfdf062d4248804
evanhosni <evankhaledhosni@gmail.com>
2024-08-27 23:14:41 -0700

    condenses createColliders

--- src/objects/colliders/createColliders.ts
+++ src/objects/colliders/createColliders.ts
@@ -14,107 +14,46 @@ export const createColliders = async (gltf: GLTF, scale: THREE.Vector3Tuple, rot
 
   for (const child of gltf.scene.children) {
     if (child instanceof THREE.Mesh && child.geometry) {
-      // if (!child.userData.collision) continue;
-
       const mesh = child.clone();
-
       mesh.rotation.set(rotation[0], rotation[1], rotation[2]);
       mesh.scale.multiply(new THREE.Vector3(...scale));
 
+      let colliderPromise: Promise<any> | null = null;
+
       if (child.userData.capsule) {
-        const collider = await createCapsuleCollider(mesh);
-        capsuleColliders.push(collider);
-        continue;
+        colliderPromise = createCollider(mesh, COLLIDER_TYPE.CAPSULE);
+        capsuleColliders.push(await colliderPromise);
+      } else if (child.userData.sphere) {
+        colliderPromise = createCollider(mesh, COLLIDER_TYPE.SPHERE);
+        sphereColliders.push(await colliderPromise);
+      } else if (child.userData.box) {
+        colliderPromise = createCollider(mesh, COLLIDER_TYPE.BOX);
+        boxColliders.push(await colliderPromise);
+      } else {
+        colliderPromise = createCollider(mesh, COLLIDER_TYPE.TRIMESH);
+        trimeshColliders.push(await colliderPromise);
       }
-
-      if (child.userData.sphere) {
-        const collider = await createSphereCollider(mesh);
-        sphereColliders.push(collider);
-        continue;
-      }
-
-      if (child.userData.box) {
-        const collider = await createBoxCollider(mesh);
-        boxColliders.push(collider);
-        continue;
-      }
-
-      const collider = await createTrimeshCollider(mesh);
-      trimeshColliders.push(collider);
     }
   }
 
   return { capsuleColliders, sphereColliders, boxColliders, trimeshColliders };
 };
 
-export const createCapsuleCollider = (mesh: THREE.Mesh): Promise<any> => {
+const createCollider = (mesh: THREE.Mesh, type: COLLIDER_TYPE): Promise<any> => {
   return new Promise((resolve) => {
     colliderWorker.onmessage = (event) => {
       resolve(event.data);
     };
 
     const params = {
-      geometry: mesh.geometry.toJSON(),
+      geometry: type === COLLIDER_TYPE.TRIMESH ? undefined : mesh.geometry.toJSON(),
+      positions: type === COLLIDER_TYPE.TRIMESH ? Array.from(mesh.geometry.attributes.position.array) : undefined,
+      index: type === COLLIDER_TYPE.TRIMESH && mesh.geometry.index ? Array.from(mesh.geometry.index.array) : null,
       position: [mesh.position.x, mesh.position.y, mesh.position.z],
       scale: [mesh.scale.x, mesh.scale.y, mesh.scale.z],
       rotation: [mesh.rotation.x, mesh.rotation.y, mesh.rotation.z],
     };
 
-    colliderWorker.postMessage({ type: COLLIDER_TYPE.CAPSULE, params });
-  });
-};
-
-export const createSphereCollider = (mesh: THREE.Mesh): Promise<any> => {
-  return new Promise((resolve) => {
-    colliderWorker.onmessage = (event) => {
-      resolve(event.data);
-    };
-
-    const params = {
-      geometry: mesh.geometry.toJSON(),
-      position: [mesh.position.x, mesh.position.y, mesh.position.z],
-      scale: [mesh.scale.x, mesh.scale.y, mesh.scale.z],
-      rotation: [mesh.rotation.x, mesh.rotation.y, mesh.rotation.z],
-    };
-
-    colliderWorker.postMessage({ type: COLLIDER_TYPE.SPHERE, params });
-  });
-};
-
-export const createBoxCollider = (mesh: THREE.Mesh): Promise<any> => {
-  return new Promise((resolve) => {
-    colliderWorker.onmessage = (event) => {
-      resolve(event.data);
-    };
-
-    const params = {
-      geometry: mesh.geometry.toJSON(),
-      position: [mesh.position.x, mesh.position.y, mesh.position.z],
-      scale: [mesh.scale.x, mesh.scale.y, mesh.scale.z],
-      rotation: [mesh.rotation.x, mesh.rotation.y, mesh.rotation.z],
-    };
-
-    colliderWorker.postMessage({ type: COLLIDER_TYPE.BOX, params });
-  });
-};
-
-export const createTrimeshCollider = (mesh: THREE.Mesh): Promise<any> => {
-  return new Promise((resolve) => {
-    const position = mesh.geometry.attributes.position.array;
-    const index = mesh.geometry.index ? mesh.geometry.index.array : null;
-
-    colliderWorker.onmessage = (event) => {
-      resolve(event.data);
-    };
-
-    const params = {
-      positions: Array.from(position),
-      index: index ? Array.from(index) : null,
-      position: [mesh.position.x, mesh.position.y, mesh.position.z],
-      rotation: [mesh.rotation.x, mesh.rotation.y, mesh.rotation.z],
-      scale: [mesh.scale.x, mesh.scale.y, mesh.scale.z],
-    };
-
-    colliderWorker.postMessage({ type: COLLIDER_TYPE.TRIMESH, params });
+    colliderWorker.postMessage({ type, params });
   });
 };
